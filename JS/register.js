e3bd463449ae0b7c3d2b3d7283c33f93f2159014
Himani Shah <53009277+Himani03@users.noreply.github.com>
2023-09-14 21:42:12 +0530

    Update register.js

--- JS/register.js
+++ JS/register.js
@@ -1,33 +1,39 @@
 let registrationHandler = () => {
   let newUser;
   let userString = JSON.parse(localStorage.getItem("usersList"));
-
-  //replacement ID
   let id = Date.now();
   let name = document.getElementById("userName").value;
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
   let confirmPassword = document.getElementById("confirmPassword").value;
   let isValidEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+
   //General Inputs validation
-    if (isValidEmail.test(email) === false) {
+  if (isValidEmail.test(email) === false) {
     alert("Enter a Valid Email");
     return false;
-  } else if (password !== confirmPassword) {
+  } else if (password != confirmPassword) {
     alert("Passwords Do not Match!");
     return false;
   } else {
     //storing values if all validations are passed
-    newUser = {
+    newUser = [{
       id: id,
       name: name,
       email: email,
       password: password,
-    };
-    //Converting the Array of Objects to String
-    userString.push(newUser);
-    userString = JSON.stringify(userString);
-    localStorage.setItem("usersList", userString);
-    return true;
+    }];
+    if (userString == null) {
+      userString = JSON.stringify(newUser);
+      localStorage.setItem("usersList", userString);
+      return true;
+    }
+    else {
+      userString.push(newUser);
+      userString = JSON.stringify(userString);
+      alert(userString);
+      localStorage.setItem("usersList", userString);
+      return true;
+    }
   }
 };
